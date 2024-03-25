e1577b8a23db2e25e92ad79e947e5583ddcc0b20
ShaikAbdul01 <shaikabdulahad2001@gmail.com>
2024-03-25 12:56:44 +0600

    Max heap

--- 9.Heap.c
+++ 9.Heap.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+/* #include <stdio.h>
 void insertHeap(int heap[], int *size, int element)
 {
     int i = (*size)++;
@@ -27,5 +27,46 @@ int main()
             printf("%d ", heap[i]);
         }
     }
+    return 0;
+}
+ */
+
+#include <stdio.h>
+
+#define MAX_HEAP_SIZE 100
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+int main()
+{
+    int n, value, heapSize = 0;
+    int heap[MAX_HEAP_SIZE];
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter the elements: ");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &value);
+        int current = heapSize;
+        heap[heapSize++] = value;
+
+        while (current > 0 && heap[current] > heap[(current - 1) / 2])
+        {
+            swap(&heap[current], &heap[(current - 1) / 2]);
+            current = (current - 1) / 2;
+        }
+    }
+
+    printf("Max Heap: ");
+    for (int i = 0; i < heapSize; i++)
+    {
+        printf("%d ", heap[i]);
+    }
+
     return 0;
 }
